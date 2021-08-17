3651f4c3d8542ad4c44f098148762995fd1ffd4b
unknown <tru17189@uvg.edu.gt>
2021-08-17 11:49:10 -0600

    Actualizacion a CCO

---
+++ gestionador_de_informacion.py
@@ -0,0 +1,31 @@
+lista = []
+listaMedia = []
+contador = 0
+listaFinal = []
+respuesta = ""
+
+with open('INFO.txt') as f:
+    lines = f.readlines()
+
+for i in lines:
+    lista.append(i)
+
+for i in lista:
+    listaMedia.append(i)
+
+for u in listaMedia:
+    for i in lista:
+        if u == i:
+            contador += 1
+        if contador >= 2:
+            if " " in u:
+                u = u.replace(" ", "")
+            if u not in listaFinal:
+                listaFinal.append(u)
+            contador = 0
+
+for u in listaFinal:
+    respuesta = '"%s",' % u
+    if "\n" in respuesta:
+        respuesta = respuesta.replace("\n", "")
+    print(respuesta)
